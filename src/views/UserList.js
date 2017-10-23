aadd757be36817184aad6b12a3c09311c0b98ad8
SreeChaitanya <sreechaitanya.y@gmail.com>
2017-10-23 16:15:41 +0530

    Changed components to ES6 classes

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -2,11 +2,18 @@
 var m = require("mithril")
 var User = require("../models/User")
 
-module.exports = {
-    oninit: User.loadList,
-    view: function() {
-        return m(".user-list", User.list.map(function(user) {
-            return m("a.user-list-item", {href: "/edit/" + user.id, oncreate: m.route.link}, user.firstName + " " + user.lastName)
-        }))
-    }
+class UserListComponent{
+  constructor(vnode) {
+        // vnode.state is undefined at this point
+        this.kind = "ES6 class"
+  }
+  oninit(){
+     User.loadList()
+  }
+  view(){
+    return m(".user-list", User.list.map(function(user) {
+        return m("a.user-list-item", {href: "/edit/" + user.id, oncreate: m.route.link}, user.firstName + " " + user.lastName)
+    }))
+  }
 }
+module.exports = UserListComponent
